46168dfd053b6bfaeb8359698316738c920ea0e6
braden <bradensad@gmail.com>
2022-12-06 14:05:15 -0600

    Update main.py

--- day6/main.py
+++ day6/main.py
@@ -5,13 +5,13 @@ def find(input):
     x=[]
     for i in input:
         if i not in x and input.count(i)>1: x.append(i)
-    return (" ".join(x))
+    return True if (" ".join(x)) == '' else False
 
 for i in range(2):
     for code in group:
         found = False ; count = 0 ; chars = ''
         while not found:
             for i in range(4 if part == 1 else 14): chars += code[i + count]
-            if(find(chars) == ''): print(f'part {part} --> {count + (4 if part == 1 else 14)}'); found = True
+            if(find(chars)): print(f'part {part} --> {count + (4 if part == 1 else 14)}'); found = True
             else: count += 1 ; chars = ''
     part = 2
