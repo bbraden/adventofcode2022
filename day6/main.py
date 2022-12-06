7556cb811150279fccb8d52bb183df735fcbcff2
braden <bradensad@gmail.com>
2022-12-06 14:00:01 -0600

    Create main.py

---
+++ day6/main.py
@@ -0,0 +1,17 @@
+with open('input.txt', 'r') as a: group = [line.strip() for line in a]
+part = 1
+
+def find(input):
+    x=[]
+    for i in input:
+        if i not in x and input.count(i)>1: x.append(i)
+    return (" ".join(x))
+
+for i in range(2):
+    for code in group:
+        found = False ; count = 0 ; chars = ''
+        while not found:
+            for i in range(4 if part == 1 else 14): chars += code[i + count]
+            if(find(chars) == ''): print(f'part {part} --> {count + (4 if part == 1 else 14)}'); found = True
+            else: count += 1 ; chars = ''
+    part = 2
